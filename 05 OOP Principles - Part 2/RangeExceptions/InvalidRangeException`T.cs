5b94a7269ea08bad4f89c54f985df9d0ad650d8c
vasilvalkov <vasil.valkov@gmail.com>
2017-01-12 22:17:40 +0200

    Range Exceptions - solved

---
+++ 05 OOP Principles - Part 2/RangeExceptions/InvalidRangeException`T.cs
@@ -0,0 +1,25 @@
+﻿namespace RangeExceptions
+{
+    using System;
+
+    public class InvalidRangeException<T> : ApplicationException
+    {
+        public InvalidRangeException(string msg, T rangeFrom, T rangeTo)
+            : base(msg)
+        {
+            this.From = rangeFrom;
+            this.To = rangeTo;
+        }
+
+        public T From { get; set; }
+        public T To { get; set; }
+
+        public override string Message
+        {
+            get
+            {
+                return string.Format("{0} [{1} - {2}]", base.Message, From, To);
+            }
+        }
+    }
+}
---
+++ 05 OOP Principles - Part 2/RangeExceptions/Startup.cs
@@ -0,0 +1,50 @@
+﻿namespace RangeExceptions
+{
+    using System;
+    using System.Globalization;
+
+    class Startup
+    {
+        static void Main()
+        {
+            try
+            {
+                int fromNumber = 1;
+                int toNumber = 100;
+
+                Console.WriteLine("Please enter a number outside the range: 1 - 100");
+
+                int input = int.Parse(Console.ReadLine());
+
+                if (input < 1 || input > 100)
+                {
+                    throw new InvalidRangeException<int>("Number must be in the range", fromNumber, toNumber);
+                }
+            }
+            catch (InvalidRangeException<int> ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                DateTime fromDate = new DateTime(2017, 1, 1);
+                DateTime toDate = new DateTime(2017, 1, 10);
+
+                Console.WriteLine("Please enter a date in the format dd.mm.yyyy outside the range: 1.1.1980 - 31.12.2013");
+
+                DateTime inputDate;
+                    bool parseInput = DateTime.TryParseExact(Console.ReadLine(), "dd:MM:yyyy", CultureInfo.CurrentCulture, DateTimeStyles.AllowLeadingWhite, out inputDate);
+                
+                if (inputDate < fromDate || inputDate > toDate)
+                {
+                    throw new InvalidRangeException<DateTime>("Date must be in the range", fromDate, toDate);
+                }
+            }
+            catch (InvalidRangeException<DateTime> ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
